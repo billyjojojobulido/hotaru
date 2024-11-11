4aaacbd1f3d5e0aa7ef3e58c64d38d44de78a710
Baocheng Wang <baocheng0121@gmail.com>
2024-11-11 22:02:03 +1100

    - fixed the audio file path error

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ from pydub import AudioSegment
 from pydub.playback import play
 from audio_manager.utils import play_audio_segment
 
-TEST_AUDIO_FILE_NAME = "test_audio_sample.mp3"
+TEST_AUDIO_FILE_NAME = "resources/test/test_audio_sample.mp3"
 
 if __name__ == "__main__":
     # 加载音频文件
