1768dcce93705c770b359326ecbc05b35843c378
Baocheng Wang <baocheng0121@gmail.com>
2024-11-11 22:47:07 +1100

    - add try-catch when fetching configration file

--- audio_manager/utils.py
+++ audio_manager/utils.py
@@ -3,19 +3,23 @@ from pydub.playback import play
 from resources.audio_info import AudioInfo
 import json
 
-CONFIG_FILE_NAME = "resources/test/audio_config.json"
+CONFIG_FILE_NAME = "resources/test/audio_config1.json"
 
 def fetch_audio_info(id: int) -> AudioInfo:
     # 读取 JSON 文件
-    with open(CONFIG_FILE_NAME, 'r') as file:
-        audio_data = json.load(file)
-
-    # 输出 JSON 数据的内容
-    for audio in audio_data:
-        if audio["id"] == id:
-            return AudioInfo(id, audio["file_name"], audio["breakpoints"])
-        
-    return None
+    try:
+        with open(CONFIG_FILE_NAME, 'r') as file:
+            audio_data = json.load(file)
+        # 输出 JSON 数据的内容
+        for audio in audio_data:
+            if audio["id"] == id:
+                return AudioInfo(id, audio["file_name"], audio["breakpoints"])
+    except FileNotFoundError:
+        print("Config File: {} not found".format(CONFIG_FILE_NAME))
+        return None
+    except Exception as e:
+        print("Error occurs during configuration".format(e))
+        return None
 
 def get_audio_media(audio_file):
     # 获取音频对象
