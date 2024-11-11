507ce06e1a4f660d91e632c6988c48e3b396721c
Baocheng Wang <baocheng0121@gmail.com>
2024-11-11 22:13:52 +1100

    - able to read JSON file now

--- audio_manager/utils.py
+++ audio_manager/utils.py
@@ -1,5 +1,23 @@
 from pydub import AudioSegment
 from pydub.playback import play
+import json
+
+CONFIG_FILE_NAME = "resources/test/audio_config.json"
+
+def fetch_audio_info():
+    # 读取 JSON 文件
+    with open(CONFIG_FILE_NAME, 'r') as file:
+        audio_data = json.load(file)
+
+    # 输出 JSON 数据的内容
+    for audio in audio_data:
+        print(f"ID: {audio['id']}")
+        print(f"Name: {audio['name']}")
+        print(f"Timestamps: {audio['breakpoints']}")
+        print()  # 打印空行分隔每个音频的输出
+
+
+
 
 def play_audio_segment(audio_file):
     # 获取音频对象
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from pydub import AudioSegment
 from pydub.playback import play
-from audio_manager.utils import play_audio_segment
+from audio_manager.utils import play_audio_segment, fetch_audio_info
 
 TEST_AUDIO_FILE_NAME = "resources/test/test_audio_sample.mp3"
 
 if __name__ == "__main__":
     # 加载音频文件
-    play_audio_segment(TEST_AUDIO_FILE_NAME)
-    
+    # play_audio_segment(TEST_AUDIO_FILE_NAME)
+    fetch_audio_info()
