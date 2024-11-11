6dbe6f856e4fc58db536dddbb47b44c57759e53e
Baocheng Wang <baocheng0121@gmail.com>
2024-11-11 21:49:47 +1100

    - tested case: start from beginning to some random time point

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     audio = AudioSegment.from_file(TEST_AUDIO_FILE_NAME)
 
     # 定义开始和结束时间（以毫秒为单位）
-    start_time = 12 * 1000
+    start_time = 0
     end_time = 15 * 1000
 
     # 裁剪音频文件
