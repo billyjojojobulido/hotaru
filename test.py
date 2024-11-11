b03c505f2617fce7aa3526922379af41297ff663
Baocheng Wang <baocheng0121@gmail.com>
2024-11-11 21:48:55 +1100

    - tested case: start from beginning to end

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
+from pydub import AudioSegment
+from pydub.playback import play
 
-
+TEST_AUDIO_FILE_NAME = "test_audio_sample.mp3"
 
 if __name__ == "__main__":
-    print("Hello World")
+    # 加载音频文件
+    audio = AudioSegment.from_file(TEST_AUDIO_FILE_NAME)
+
+    # 定义开始和结束时间（以毫秒为单位）
+    start_time = 12 * 1000
+    end_time = 15 * 1000
+
+    # 裁剪音频文件
+    segment = audio[start_time:end_time]
+
+    # 播放音频片段
+    play(segment)
