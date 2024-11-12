fbd9f28630f8f5bef4718a8a928aaf841d0a1855
Baocheng Wang <baocheng0121@gmail.com>
2024-11-12 21:13:02 +1100

    - fixed merge conflict

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from pydub import AudioSegment
 from pydub.playback import play
 from audio_manager.utils import play_audio_segment, fetch_audio_info, get_audio_media
 
+
 if __name__ == "__main__":
     # 加载音频文件
     # play_audio_segment(TEST_AUDIO_FILE_NAME)
