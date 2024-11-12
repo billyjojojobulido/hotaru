e0e37231263ddc39b40097bbc7433dbca211192a
Baocheng Wang <baocheng0121@gmail.com>
2024-11-12 20:51:25 +1100

    #20 case 1 pass

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from pydub import AudioSegment
 from pydub.playback import play
 from audio_manager.utils import play_audio_segment, fetch_audio_info, get_audio_media
 
-TEST_AUDIO_FILE_NAME = "resources/test/test_audio_sample.mp3"
 
 if __name__ == "__main__":
     # 加载音频文件
