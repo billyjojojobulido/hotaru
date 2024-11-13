ef69e167314e0763bae641630e3823bb0215c666
Baocheng Wang <baocheng0121@gmail.com>
2024-11-14 00:04:14 +1100

    - added some attr for the class

--- classes/audio_node.py
+++ classes/audio_node.py
@@ -1,3 +1,12 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+
 class AudioSegmentNode:
-    def __init__(self, id=0):
-        pass
+    def __init__(self, audio_seg: AudioSegment, order, next=None):
+        self._audio_seg = audio_seg
+        self._ord = order
+        self._next = next
+
+    def link(self, next_node):
+        self._next = next_node
