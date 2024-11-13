392c9707abb0618695f364eb9d6c116e2c9cc925
Baocheng Wang <baocheng0121@gmail.com>
2024-11-14 00:05:30 +1100

    - added the func to determine if the node is head

--- classes/audio_node.py
+++ classes/audio_node.py
@@ -1,6 +1,7 @@
 from pydub import AudioSegment
 from pydub.playback import play
 
+HEAD_ID = 0
 
 class AudioSegmentNode:
     def __init__(self, audio_seg: AudioSegment, order, next=None):
@@ -14,3 +15,6 @@ class AudioSegmentNode:
 
     def next(self):
         return self._next
+
+    def is_head(self):
+        return self._ord == HEAD_ID
