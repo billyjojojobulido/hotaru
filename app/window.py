f847296addf83fe1630782fd5bec1a0fe8a345f0
Baocheng Wang <baocheng0121@gmail.com>
2024-11-14 23:38:21 +1100

    - adjust the size of window components

--- app/window.py
+++ app/window.py
@@ -11,7 +11,7 @@ class Window:
         # attr初始化
         self.root = Tk()
         self.init_window_frame = LabelFrame(self.root, text="")
-        self.init_window_frame.place(relx=0.1, rely=0.15, relwidth=0.8, relheight=0.7)
+        self.init_window_frame.place(relx=0.1, rely=0.075, relwidth=0.8, relheight=0.85)
 
         # 数据变量
         self.audio_combo_list = None    # 机经音频选择下拉框
@@ -19,8 +19,9 @@ class Window:
 
         # 设置根窗口属性
         self.root.title("Hotaru")
-        self.root.geometry("600x400")
         self.root.attributes("-alpha", 1)
+
+        self.center_window(600, 400)
 
         self.create_page()
 
@@ -62,12 +63,15 @@ class Window:
     """
     调整屏幕大小 和 位置
     """
-    def center_window(root, width, height):
+    def center_window(self, width, height):
         # 获取屏幕尺寸以及计算位置
-        screen_width = root.winfo_screenwidth()
-        screen_height = root.winfo_screenheight()
+        screen_width = self.root.winfo_screenwidth()
+        screen_height = self.root.winfo_screenheight()
         x = (screen_width - width) // 2
         y = (screen_height - height) // 2
+
+        self.root.geometry(f'{width}x{height}+{x}+{y}')
+
 
     # 获取音频下拉框的内容 (str)
     def get_audio_id(self, event):
