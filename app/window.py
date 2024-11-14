fef0ea1c522ff2e2e81463d444a00741f1174d76
Baocheng Wang <baocheng0121@gmail.com>
2024-11-14 22:57:45 +1100

    - temp save :: make entry non writeable

--- app/window.py
+++ app/window.py
@@ -14,8 +14,8 @@ class Window:
         self.init_window_frame.place(relx=0.1, rely=0.15, relwidth=0.8, relheight=0.7)
 
         # 数据变量
-        self.src_path = StringVar()                                 # 数据文件路径 StringVar
-        self.src_path.set(" -- e.g. 70001 -- ")
+        self.audio_combo_list = None    # 机经音频选择下拉框
+        self.audio_id = -1
 
         # 设置根窗口属性
         self.root.title("Hotaru")
@@ -32,13 +32,22 @@ class Window:
         Label(self.root).grid(row=0, stick='W', pady=10)
         # 第一行选项 - 源文件
         Label(self.init_window_frame).grid(row=1, column=0, padx=30)
-        Label(self.init_window_frame, text="请选择机经序号").grid(row=1, column=1, stick='W', pady=10)
-        self.src_entry = Entry(
+        Label(self.init_window_frame, text="请选择机经序号: ").grid(row=1, column=1, stick='W', pady=10)
+
+        # self.src_entry.grid(row=1, column=1, stick='E')
+
+        self.audio_combo_list = ttk.Combobox(
             self.init_window_frame,
-            textvariable=self.src_path,
+            textvariable=self.audio_id,
             width=20,
         )
-        self.src_entry.grid(row=1, column=2, stick='E')
+        self.audio_combo_list['values'] = (
+            "70001", "70002", "70003", "70004", "70090"
+        )
+        self.audio_combo_list.current(0)   # 默认选择"一月"
+        # self.audio_combo_list.bind("<<ComboboxSelected>>", self.get_month)
+
+        self.audio_combo_list.grid(row=1, column=2, stick='W')
 
     def run(self):
         self.init_window_frame.mainloop()
