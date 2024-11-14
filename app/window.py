dbff502903dc01c3e861313ddda6ac3f06f63aec
Baocheng Wang <baocheng0121@gmail.com>
2024-11-14 22:48:46 +1100

    - added the id entry on the ui

--- app/window.py
+++ app/window.py
@@ -13,6 +13,10 @@ class Window:
         self.init_window_frame = LabelFrame(self.root, text="")
         self.init_window_frame.place(relx=0.1, rely=0.15, relwidth=0.8, relheight=0.7)
 
+        # 数据变量
+        self.src_path = StringVar()                                 # 数据文件路径 StringVar
+        self.src_path.set(" -- e.g. 70001 -- ")
+
         # 设置根窗口属性
         self.root.title("Hotaru")
         self.root.geometry("600x400")
@@ -26,7 +30,15 @@ class Window:
     """
     def create_page(self):
         Label(self.root).grid(row=0, stick='W', pady=10)
-
+        # 第一行选项 - 源文件
+        Label(self.init_window_frame).grid(row=1, column=0, padx=30)
+        Label(self.init_window_frame, text="请选择机经序号").grid(row=1, column=1, stick='W', pady=10)
+        self.src_entry = Entry(
+            self.init_window_frame,
+            textvariable=self.src_path,
+            width=20,
+        )
+        self.src_entry.grid(row=1, column=2, stick='E')
 
     def run(self):
         self.init_window_frame.mainloop()
