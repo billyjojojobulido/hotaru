cd7380f41ee717315d4414aa83b2da7c9be4ef74
Baocheng Wang <baocheng0121@gmail.com>
2024-11-17 21:43:09 +1100

    - minimal viable ui

--- app/window_01.py
+++ app/window_01.py
@@ -73,14 +73,8 @@ class App(customtkinter.CTk):
         self.slider_progressbar_frame.grid(row=1, column=0, padx=(20, 0), pady=(20, 0), sticky="nsew")
         self.slider_progressbar_frame.grid_columnconfigure(0, weight=1)
         self.slider_progressbar_frame.grid_rowconfigure(4, weight=1)
-        self.seg_button_1 = customtkinter.CTkSegmentedButton(self.slider_progressbar_frame)
-        self.seg_button_1.grid(row=0, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
         self.progressbar_2 = customtkinter.CTkProgressBar(self.slider_progressbar_frame)
         self.progressbar_2.grid(row=2, column=0, padx=(20, 10), pady=(10, 10), sticky="ew")
-        self.slider_2 = customtkinter.CTkSlider(self.slider_progressbar_frame, orientation="vertical")
-        self.slider_2.grid(row=0, column=0, rowspan=5, padx=(10, 10), pady=(10, 10), sticky="ns")
-        self.progressbar_3 = customtkinter.CTkProgressBar(self.slider_progressbar_frame, orientation="vertical")
-        self.progressbar_3.grid(row=0, column=1, rowspan=5, padx=(10, 20), pady=(10, 10), sticky="ns")
 
         # create scrollable frame
         # self.scrollable_frame = customtkinter.CTkScrollableFrame(self, label_text="CTkScrollableFrame")
@@ -98,10 +92,7 @@ class App(customtkinter.CTk):
         # self.scrollable_frame_switches[0].select()
         # self.scrollable_frame_switches[4].select()
         # self.optionmenu_1.set("CTkOptionmenu")
-        self.slider_2.configure(command=self.progressbar_3.set)
         self.textbox.insert("0.0", "")
-        self.seg_button_1.configure(values=["CTkSegmentedButton", "Value 2", "Value 3"])
-        self.seg_button_1.set("Value 2")
 
 
     def _init_audio_option_menu(self):
