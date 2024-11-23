0130f449eeadbfcc647ec031600d71bb8810edfb
Baocheng Wang <baocheng0121@gmail.com>
2024-11-24 02:08:56 +1100

    - added the lock and unlock function to the console && make it readonly when not updating

--- component/console.py
+++ component/console.py
@@ -7,18 +7,29 @@ class Console(customtkinter.CTkTextbox):
 
     def __init__(self, root: customtkinter.CTk):
         super().__init__(root, font=FONT)
+        self._lock()
+    
+    def _lock(self):
+        super().configure(state="disabled")
+
+    def _unlock(self):
+        super().configure(state="normal")
 
 
     def console_log(self,  msg: str, info_type="INFO"):
         if not msg or len(msg) == 0:
             return
+        self._unlock()
         super().insert('end', "\n%s [%s] %s" % (time.strftime('%Y-%m-%d %H:%M:%S'), info_type, msg))
         super().update()
+        self._lock()
 
 
     def console_log_error(self, err: str):
         if not err or len(err) == 0:
             return
+        self._unlock()
         super().configure(text_color="red")
         super().insert('end', "\n%s [%s] %s" % (time.strftime('%Y-%m-%d %H:%M:%S'), err, "ERROR"))
         super().update()
+        self._unlock()
